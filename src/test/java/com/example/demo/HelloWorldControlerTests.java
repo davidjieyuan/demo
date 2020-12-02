d3296036cc49014ec254d2dc3bb6e0b375b8d516
davidyuan <yuanjie_7@hotmail.com>
2020-12-02 14:47:35 +0800

    Initial commit "first"

---
+++ src/test/java/com/example/demo/HelloWorldControlerTests.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import com.example.demo.controller.CustomerController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@SpringBootTest
+public class HelloWorldControlerTests {
+    private MockMvc mvc;
+    @Autowired
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new CustomerController()).build();
+    }
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+    //编写简单的http请求来测试；使用mockmvc进行，利用MockMvcResultHandlers.print()打印出执行结果
+
+}
